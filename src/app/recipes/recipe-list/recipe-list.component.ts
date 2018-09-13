d3560708c1c02557c8122a813cadd6a08890d73d
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2018-09-13 10:39:20 +0200

    Add core directory to project structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Recipe } from '../../core/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('First recipe', ' Test', 'https://i.ytimg.com/vi/PPLa3yIAoUo/maxresdefault.jpg')
+    new Recipe('First recipe', 'Test', 'https://i.ytimg.com/vi/PPLa3yIAoUo/maxresdefault.jpg'),
+    new Recipe('Second recipe', 'Test2', 'https://cms.splendidtable.org/sites/default/files/styles/w2000/public/Burger-Lab_Lamb-Burger-LEDE.jpg?itok=V_i57ev6')
   ];
 
   constructor() { }
