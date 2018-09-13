78e11edeb349c278478f8b52550913987085d400
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2018-09-13 13:13:30 +0200

    Add template and example data to shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../core/models/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Potatos', 10, 'kg'),
+    new Ingredient('Bread', 1)
+  ];
 
   constructor() { }
 
