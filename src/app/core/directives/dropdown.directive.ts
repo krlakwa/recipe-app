724fa7c1eb14259961ac912e9704ab91599004b4
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2018-09-18 11:01:22 +0200

    Update recipe-detail component

--- src/app/core/directives/dropdown.directive.ts
+++ src/app/core/directives/dropdown.directive.ts
@@ -5,7 +5,6 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-  
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
